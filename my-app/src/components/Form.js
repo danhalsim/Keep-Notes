3d12ad28d2eabaab1b8000617c9ca1b7839486fd
dansim6935 <dansim6935@gmail.com>
2023-11-07 02:32:13 -0800

    Added usestate logic for the form fields

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,19 +1,42 @@
 import { Fab, Paper, TextField } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  function changeHandler(event) {
+    const {name, value} = event.target;
+    setNote((preValues) => {
+      return{
+        ...preValues,
+        [name]: value,
+      }
+    })
+  }
   return (
     <Paper style={{ margin: "10px 10%", padding: "25px 50px" }}>
       <form>
-        <TextField label="Title" fullWidth autoComplete="off" />
         <TextField
+          onChange={changeHandler}
+          name="title"
+          value={note.title}
+          label="Title"
+          fullWidth
+          autoComplete="off"
+        />
+        <TextField
+          onChange={changeHandler}
+          name="content"
+          value={note.content}
           label="Content"
           fullWidth
           autoComplete="off"
           multiline
           rows={4}
         />
-        <Fab style={{marginTop:"20px"}}>
+        <Fab style={{ marginTop: "20px" }}>
           <svg
             class="w-6 h-6 text-gray-800 dark:text-white"
             aria-hidden="true"
